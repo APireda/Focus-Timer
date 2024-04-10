e0f28e0ec31711d8df2a6d256e9dc0bc0dbc1f89
Ayrton Pireda <piredaayrton@gmail.com>
2024-04-10 16:43:35 -0300

    Revert "first commite"
    This reverts commit 74f7059f05ec237b563632bd9e703a73ca53f62c.

--- js/sounds.js
+++
@@ -1,7 +0,0 @@
-export const alarmSound = new Audio('../assets/kichen-timer.mp3')
-
-export const buttonSound = new Audio('../assets/button-press.wav')
-
-export const backgroundMusic = new Audio('../assets/bg-audio.mp3')
-
-backgroundMusic.loop = true
