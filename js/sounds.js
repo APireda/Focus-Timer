cadc189685f06054affb4fe5da0dcc14d5deef23
Ayrton Pireda <piredaayrton@gmail.com>
2024-04-10 16:38:36 -0300

    first commite

---
+++ js/sounds.js
@@ -0,0 +1,7 @@
+export const alarmSound = new Audio('../assets/kichen-timer.mp3')
+
+export const buttonSound = new Audio('../assets/button-press.wav')
+
+export const backgroundMusic = new Audio('../assets/bg-audio.mp3')
+
+backgroundMusic.loop = true
